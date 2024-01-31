47e3226e154e54af1e19c218dd9f1e801e172b19
Kickblip <wyatt.zilker@gmail.com>
2024-01-31 15:22:02 -0700

    rpa 3

--- testing/Ascii.java
+++ testing/Ascii.java
@@ -3,20 +3,15 @@ public class Ascii {
     
     
 
-    public static String formatterA(double val) {
+  public static boolean signCheck(int val) {
+    return 10 == val;
+ }
+ 
+ 
+ public static void main(String[] args) {
+    System.out.printf("%b, %b, %b", signCheck(10), signCheck(0), signCheck(-10));
+ }
 
-
-        return String.format("%.2f%%,%2d", val, (int) val);
-        
-        }
-        
-         
-        
-        public static void main(String[] args) {
-        
-          System.out.println(formatterA(10/3.0));
-        
-        }
         
         
         
