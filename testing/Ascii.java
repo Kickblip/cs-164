c8b60d65c84520adc85b1dc009ef8972d6f4ccb6
Kickblip <wyatt.zilker@gmail.com>
2024-02-12 21:57:26 -0700

    testing

--- testing/Ascii.java
+++ testing/Ascii.java
@@ -1,19 +1,8 @@
 package testing;
+
 public class Ascii {
-    
-    
+    public static void main(String[] args) {
+        System.out.println(10/2 > 5 || (3+4) < 10 && true);
 
-  public static boolean signCheck(int val) {
-    return 10 == val;
- }
- 
- 
- public static void main(String[] args) {
-    System.out.printf("%b, %b, %b", signCheck(10), signCheck(0), signCheck(-10));
- }
-
-        
-        
-        
- 
+    }   
 }
