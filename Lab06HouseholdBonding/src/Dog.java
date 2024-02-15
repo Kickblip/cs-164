730dd4757faf65cb81a13a913178d96ec09dba4c
Kickblip <wyatt.zilker@gmail.com>
2024-02-15 15:29:01 -0700

    lab 6

---
+++ Lab06HouseholdBonding/src/Dog.java
@@ -0,0 +1,33 @@
+public class Dog {
+    private String name;
+    private int age;
+    private String breed;
+
+    public Dog(String name, int age, String breed){
+        setName(name);
+        setAge(age);
+        setBreed(breed);
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public void setBreed(String breed){
+        this.breed = breed;
+    }
+    public String getBreed(){
+        return this.breed;
+    }
+    public static void main(String[] args) {
+        Dog Buster = new Dog("Buster", 4, "Golden Retriever");
+        System.out.println(String.format("Name: %s\nAge: %d\nBreed: %s", Buster.getName(), Buster.getAge(), Buster.getBreed()));
+    }
+}
