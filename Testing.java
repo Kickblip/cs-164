d8ce9baf983b849865f790fd75333a4c078a2ae9
Kickblip <wyatt.zilker@gmail.com>
2024-02-27 12:18:35 -0700

    rpa 7

--- Testing.java
+++ Testing.java
@@ -1,49 +1,18 @@
 public class Testing {
     public static void main (String[] args) {
-        System.out.println(sub2("frankie@rhps.com"));
-        System.out.println(sub3("Kakwit"));
-        System.out.println(sub("Brad,Magenta,Columbia"));
+        checkTwo();
 
     }
 
 
-    public static String sub(String str) {
+    public static void checkTwo() {
+        for (char c = 'A'; c < 'D'; c++) {
+            for (int i = 0; i <= 2; i++) {
+                System.out.println("seat " + c + i);
+            }
+        }
+  }
 
-        return str.substring(str.indexOf(",") + 1, str.indexOf(",", str.indexOf(",") + 1));
-
-    }
-
-
-
-    public static String sub2(String str) {
-
-        return str.substring(str.indexOf("@"));
-
-    }
-
-
-
-    public static String sub3(String str) {
-
-        String rtn = "";
-
-        String key = "KitHawk";
-
-        String key2 = "abcdefgh";
-
-
-
-        rtn += key2.charAt(key.indexOf(str.charAt(0)));
-
-        rtn += key2.charAt(key.indexOf(str.charAt(1)));
-
-        rtn += key2.charAt(key.indexOf(str.charAt(2)));
-
-
-
-        return rtn;
-
-    }
 
 
 }
