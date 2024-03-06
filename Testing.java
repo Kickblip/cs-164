15893ffadecb4850e5c45891607d06c116f84087
Kickblip <wyatt.zilker@gmail.com>
2024-03-06 14:46:05 -0700

    rpa testing

--- Testing.java
+++ Testing.java
@@ -1,19 +1,24 @@
 public class Testing {
     public static void main (String[] args) {
 
-        String magic = "abracadabra";
-
-        System.out.println(magic.indexOf("b"));
-        System.out.println(magic.indexOf("a"));
-        System.out.println(magic.indexOf("a", magic.indexOf("c")));
-        System.out.println(magic.indexOf(1,6));
-        System.out.println(magic.indexOf(magic.indexOf("c"))+1);
-
-        // String shorter = magic.substring(1, 6);
-        String shorter = magic.substring(1, magic.indexOf("a", magic.indexOf("c"))+1);
-        // String shorter = magic.substring(magic.indexOf("b"), magic.indexOf("a", magic.indexOf("c"))+1);
-
-        System.out.println(shorter);
+        
+    int value = 0;
+    StringBuilder build = new StringBuilder();
+    do {
+    build.append(value++);
+    } while (++value < 10);
+    
+    System.out.println(build.toString()); // print 1
+    
+    String hello = "What's----the---motto---with-you?";
+    while(hello.contains("--")) {
+    hello = hello.replaceFirst("---", "-");
+    hello = hello.replaceFirst("--", "-");
+    System.out.println(hello);
+    }
+    
+    System.out.println(hello); // print 2
+ 
 
     }
 }
