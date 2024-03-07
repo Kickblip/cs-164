20e8e625c54c7d0e2a818470cb386853a149b0a1
Kickblip <wyatt.zilker@gmail.com>
2024-03-07 13:38:39 -0700

    rpa 7

--- Testing.java
+++ Testing.java
@@ -1,24 +1,14 @@
 public class Testing {
     public static void main (String[] args) {
-
-        
-    int value = 0;
-    StringBuilder build = new StringBuilder();
-    do {
-    build.append(value++);
-    } while (++value < 10);
-    
-    System.out.println(build.toString()); // print 1
-    
-    String hello = "What's----the---motto---with-you?";
-    while(hello.contains("--")) {
-    hello = hello.replaceFirst("---", "-");
-    hello = hello.replaceFirst("--", "-");
-    System.out.println(hello);
+        checkTwo();
     }
-    
-    System.out.println(hello); // print 2
- 
 
+    public static void checkTwo() {
+        for (char c = 'A'; c < 'D'; c++) {
+            for (int i = 0; i <= 2; i++) {
+                System.out.println("seat " + c + i);
+            }
+        }
     }
+
 }
