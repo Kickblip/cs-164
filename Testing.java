ec824ac78ab7f021076a7277c93eae9388a17ea3
Kickblip <wyatt.zilker@gmail.com>
2024-04-10 14:02:06 -0600

    part 1 of in class activity

--- Testing.java
+++ Testing.java
@@ -1,10 +1,22 @@
 public class Testing {
-  public static String stringManipulator(String str) {
-    if (str.isEmpty()) return str;
-    return stringManipulator(str.substring(1)) + str.charAt(0);
-}
+
+
+    /**
+        @author Cody, Wyatt
+        @param int[] arr
+        @return int[]
+
+        Declare a new empty array called reversedArr with the same length as arr
+        Iterate through the array from 0 to arr.length (non-inclusive)
+        Set reversedArr at arr.length - 1 - i to arr at i
+    */ 
+    public static int[] reverseArray(int[] arr) {}
+
 public static void main(String[] args) {
-  System.out.println(stringManipulator("356"));
+    int[] test = reverseArray(new int[] {1, 2, 3, 4, 5});
+    for (int i : test) {
+        System.out.print(i + " ");
+    };
 }
       
 
