3f4e60e0b2f44360d69bb40e24b657e1410e23dc
Kickblip <wyatt.zilker@gmail.com>
2024-04-10 14:15:15 -0600

    part 2 of in class activity

--- Testing.java
+++ Testing.java
@@ -1,22 +1,44 @@
 public class Testing {
 
 
-    /**
-        @author Cody, Wyatt
-        @param int[] arr
-        @return int[]
+/**Problem 3
+ * @author Liam Barrett and Prajjol Neupane
+ * @param array1
+ * @return array1 with the max value swaped with the last index of the array
+ * Step-by-step algorithm
+ * Assign array1 to a temporary integer variable
+ * Create integer variable to hold the index
+ * Use a for each loop to iterate through each element i in the array1
+ * Store the temporary integer with the max value in each element in array1
+ * Swap the max value found with the last index of array1 which is length() - 1
+ *
+ */
 
-        Declare a new empty array called reversedArr with the same length as arr
-        Iterate through the array from 0 to arr.length (non-inclusive)
-        Set reversedArr at arr.length - 1 - i to arr at i
-    */ 
-    public static int[] reverseArray(int[] arr) {}
+   public static int[] SwapMaxWithLast(int[] array1) {
+    int[] temparr = array1;
+    int max = 0;
+    int idx = 0;
+    for (int i : array1) {
+        if (i > max) {
+            max = i;
+            for (int j = 0; j < array1.length; j++) {
+                if (array1[j] == max) {
+                    idx = j;
+                }
+            }
+        }
+    }
+    temparr[idx] = array1[array1.length - 1];
+    temparr[array1.length - 1] = max;
+
+    return temparr;
+   }
 
 public static void main(String[] args) {
-    int[] test = reverseArray(new int[] {1, 2, 3, 4, 5});
+    int[] test = SwapMaxWithLast(new int[] {4, 3, 5, 2, 1});
     for (int i : test) {
         System.out.print(i + " ");
-    };
+    }
 }
       
 
